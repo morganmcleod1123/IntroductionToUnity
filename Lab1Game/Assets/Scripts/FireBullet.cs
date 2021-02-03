378e7c9dfbe348cc59a75a17fd781c44b2ad351e
Morgan McLeod <morganmcleod1123@gmail.com>
2021-02-03 13:33:59 -0600

    Added Bird Sprite

--- Lab1Game/Assets/Scripts/FireBullet.cs
+++ Lab1Game/Assets/Scripts/FireBullet.cs
@@ -9,7 +9,6 @@ public class FireBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(bullet);
     }
 
     // Update is called once per frame
